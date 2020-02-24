db0093315fc89114aaaeb6373736f30dcdecc21f
MB <bergerm613@gmail.com>
2020-02-24 14:22:44 -0500

    expanded tests with NotEnoughChangeException

--- test/berger/money/MoneyTest.java
+++ test/berger/money/MoneyTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 public class MoneyTest
 {
     @Test
-    public void pay()
+    public void pay() throws NotEnoughChangeException
     {
         //given
         Cashier bob = new Cashier();
@@ -24,4 +24,30 @@ public class MoneyTest
         assertEquals(change.getTotal(), 0.51, 0.01);
     }
 
+    @Test (expected = NotEnoughChangeException.class)
+    public void payWhenBroke() throws NotEnoughChangeException
+    {
+        //given
+        Cashier bob = new Cashier();
+        Cash dollahs = new Cash();
+
+        dollahs.addOneDollars(3);
+
+        //when
+        Cash change = bob.pay(2.49, dollahs);
+    }
+
+    @Test (expected = NotEnoughChangeException.class)
+    public void payInsufChange() throws NotEnoughChangeException
+    {
+        //given
+        Cashier bob = new Cashier();
+        Cash dollahs = new Cash();
+
+        bob.register.addQuarters(100);
+        dollahs.addOneDollars(3);
+
+        //when
+        Cash change = bob.pay(2.49, dollahs);
+    }
 }
