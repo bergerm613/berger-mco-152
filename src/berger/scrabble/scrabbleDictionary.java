5ae33a7815086df42d35bdd2a0926110891883fd
MB <bergerm613@gmail.com>
2020-02-24 17:14:43 -0500

    added HashSet

--- src/berger/scrabble/scrabbleDictionary.java
+++ src/berger/scrabble/scrabbleDictionary.java
@@ -2,28 +2,38 @@ package berger.scrabble;
 
 import java.io.BufferedReader;
 import java.io.*;
+import java.util.HashSet;
+import java.util.Iterator;
 
 public class scrabbleDictionary
 {
     BufferedReader br;
+    HashSet<String> dictionaryHash;
 
     public scrabbleDictionary() throws IOException
     {
         File file = new File("C:\\Users\\michal\\Desktop\\dictionary.txt");
         br = new BufferedReader(new FileReader(file));
-    }
+        dictionaryHash = new HashSet<String>();
 
-    public boolean wordInFile(String word) throws IOException
-    {
         String st;
-
         while ((st = br.readLine()) != null)
         {
-            if ((st.split(" ")[0]).equalsIgnoreCase(word))
+            dictionaryHash.add(st.split(" ")[0]);
+        }
+    }
+
+    public boolean wordInFile(String word)
+    {
+        Iterator<String> itr = dictionaryHash.iterator();
+        while (itr.hasNext())
+        {
+            if (itr.next().equalsIgnoreCase(word))
             {
                 return true;
             }
         }
+
         return false;
     }
 }
