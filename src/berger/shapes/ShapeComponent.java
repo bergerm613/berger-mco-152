ef30320b9da85b726fe2df98a684c3c018a8e1e2
MB <bergerm613@gmail.com>
2020-03-29 13:33:34 -0400

    images now rotate

--- src/berger/shapes/ShapeComponent.java
+++ src/berger/shapes/ShapeComponent.java
@@ -5,19 +5,41 @@ import java.awt.*;
 
 public class ShapeComponent extends JComponent
 {
-
+    private int rotation = 0;
     private String shape = "";
+
+    public ShapeComponent() {
+        Thread thread = new Thread() {
+            public void run() {
+                super.run();
+                while(true) {
+                    repaint();
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        thread.start();
+    }
 
     @Override
     protected void paintComponent(Graphics g)
     {
+        super.paintComponent(g);
+
         int width = getWidth();
         int height = getHeight();
         int centerX = width / 2;
         int centerY = height / 2;
+
         g.translate(centerX, centerY);
 
-        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.rotate(Math.toRadians(rotation));
+        rotation++;
 
         if (shape.equals("glasses"))
         {
